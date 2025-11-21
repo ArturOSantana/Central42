beca28016d7ddfdaef60a25c7c204f36b85da7a1
Artur O. de Santana <artur.santana.dev@gmail.com>
2025-11-21 18:56:59 -0300

    Add BD atualizado

--- central42.sql
+++ central42.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 21/11/2025 às 19:07
+-- Tempo de geração: 21/11/2025 às 22:54
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,57 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categorias`
---
-
-CREATE TABLE `categorias` (
-  `id_categoria` int(11) NOT NULL,
-  `nome_categoria` varchar(100) NOT NULL,
-  `id_departamento` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `categorias`
---
-
-INSERT INTO `categorias` (`id_categoria`, `nome_categoria`, `id_departamento`) VALUES
-(4, 'Acesso ao Sistema', 1),
-(6, 'Benefícios', 2),
-(52, 'Benefícios e Folha', 2),
-(55, 'Compras Internas', 7),
-(5, 'Falha de Equipamento', 1),
-(8, 'Férias', 2),
-(7, 'Folha de Pagamento', 2),
-(3, 'Instalação de Software', 1),
-(56, 'Limpeza e Manutenção', 8),
-(12, 'Nota Fiscal', 3),
-(54, 'Notas Fiscais', 3),
-(15, 'Orçamento', 3),
-(13, 'Pagamentos Pendentes', 3),
-(14, 'Prestação de Contas', 3),
-(2, 'Problemas de Sistema', 1),
-(9, 'Recrutamento', 2),
-(57, 'Rede e Servidores', 1),
-(11, 'Reembolso', 3),
-(53, 'Reembolsos', 3),
-(1, 'Suporte Técnico', 1),
-(10, 'Treinamentos', 2),
-(50, 'Troca de Equipamentos', 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `chamados`
 --
 
 CREATE TABLE `chamados` (
   `id_chamado` int(11) NOT NULL,
-  `titulo` varchar(150) DEFAULT NULL,
-  `descricao` text DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  `data_abertura` datetime DEFAULT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `id_departamento` int(11) DEFAULT NULL,
+  `titulo` varchar(150) NOT NULL,
+  `descricao` text NOT NULL,
+  `status` varchar(50) DEFAULT 'Aberto',
+  `data_abertura` datetime DEFAULT current_timestamp(),
+  `id_usuario` int(11) NOT NULL,
+  `id_departamento` int(11) NOT NULL,
   `departamento_destino` int(11) NOT NULL,
   `data_fechamento` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -84,17 +44,13 @@ CREATE TABLE `chamados` (
 --
 
 INSERT INTO `chamados` (`id_chamado`, `titulo`, `descricao`, `status`, `data_abertura`, `id_usuario`, `id_departamento`, `departamento_destino`, `data_fechamento`) VALUES
-(10, 'erro ', 'Boa', 'Fechado', '2025-11-15 16:15:25', 1, 1, 1, '2025-11-15 18:12:34'),
-(11, 'Erro no pc', 'Meu PC não ta pegando ', 'Aberto', '2025-11-17 23:51:06', 5, 3, 3, NULL),
-(12, 'Erro', 'Erro', 'Aberto', '2025-11-17 23:57:18', 5, 3, 3, NULL),
-(13, 'Contrato_Estagio', 'Não recebi', 'Aberto', '2025-11-18 04:08:49', 5, 3, 3, NULL),
-(14, 'erro na cat', 'erro', 'Aberto', '2025-11-18 04:09:21', 5, 3, 3, NULL),
-(15, 'Erro', 'teste', 'Aberto', '2025-11-18 04:15:04', 1, 1, 1, NULL),
-(16, 'erro na folha', 'erro ali', 'Aberto', '2025-11-18 04:20:08', 5, 3, 3, NULL),
-(22, 'Erro', 'aoao', 'Aberto', '2025-11-18 06:32:42', 5, 2, 2, NULL),
-(23, 'Errro numero 23132', 'lokjk', 'Aberto', '2025-11-18 06:50:16', 5, 2, 2, NULL),
-(24, 'Erro na folha', 'deu ruik', 'Aberto', '2025-11-18 20:28:16', 5, 2, 2, NULL),
-(25, 'erro na folha de pagamento', 'deu ri', 'Aberto', '2025-11-18 21:48:45', 5, NULL, 2, NULL);
+(1, 'Problema com email', 'Não consigo acessar meu email corporativo', 'Aberto', '2025-11-21 16:51:27', 2, 3, 1, NULL),
+(2, 'Solicitação de férias', 'Gostaria de agendar minhas férias para dezembro', 'Aberto', '2025-11-21 16:51:27', 2, 3, 2, NULL),
+(3, 'Erro no sistema financeiro', 'O sistema está com erro ao gerar relatórios', 'Aberto', '2025-11-21 16:51:27', 3, 2, 3, NULL),
+(4, 'Erro', 'P financeiro', 'Aberto', '2025-11-21 21:33:05', 1, 1, 3, NULL),
+(5, 'Erro', 'p RH', 'Aberto', '2025-11-21 21:34:05', 1, 1, 2, NULL),
+(6, 'erro ti', 'erro no ti', 'Aberto', '2025-11-21 21:34:49', 3, 2, 1, NULL),
+(7, 'Errro numero 23132', 'nnn', 'Aberto', '2025-11-21 22:19:58', 2, 3, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -115,8 +71,8 @@ INSERT INTO `departamentos` (`id_departamento`, `nome`) VALUES
 (1, 'TI'),
 (2, 'RH'),
 (3, 'Financeiro'),
-(7, 'Administrativo'),
-(8, 'Infraestrutura');
+(4, 'Administrativo'),
+(5, 'Infraestrutura');
 
 -- --------------------------------------------------------
 
@@ -130,7 +86,7 @@ CREATE TABLE `feedbacks` (
   `id_usuario` int(11) DEFAULT NULL,
   `nota` int(11) DEFAULT NULL,
   `comentario` text DEFAULT NULL,
-  `data_feedback` datetime DEFAULT NULL
+  `data_feedback` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -150,8 +106,8 @@ CREATE TABLE `tipos_usuario` (
 
 INSERT INTO `tipos_usuario` (`id_tipo_usuario`, `nome_tipo`) VALUES
 (1, 'usuario'),
-(2, 'admin'),
-(3, 'superadmin');
+(2, 'supervisor'),
+(3, 'admin');
 
 -- --------------------------------------------------------
 
@@ -173,21 +129,13 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nome`, `email`, `senha`, `id_tipo_usuario`, `id_departamento`) VALUES
-(1, 'ARTUR OLIVEIRA DE SANTANA', 'artursantana123@gmail.com', '123', 3, 1),
-(5, 'Karina Giorgio', 'santa@gmail.com', '123', 1, 3),
-(6, 'Miryam de Araújo Macario', 'teste@gmail.com', '123', 2, 2);
+(1, 'ARTUR OLIVEIRA DE SANTANA', 'artur@email.com', '123', 3, 1),
+(2, 'Karina Giorgio', 'karina@email.com', '123', 1, 3),
+(3, 'Miryam de Araújo Macario', 'miryam@email.com', '123', 2, 2);
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices de tabela `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id_categoria`),
-  ADD UNIQUE KEY `unq_categoria_departamento` (`nome_categoria`,`id_departamento`),
-  ADD KEY `id_departamento` (`id_departamento`);
 
 --
 -- Índices de tabela `chamados`
@@ -195,7 +143,8 @@ ALTER TABLE `categorias`
 ALTER TABLE `chamados`
   ADD PRIMARY KEY (`id_chamado`),
   ADD KEY `id_usuario` (`id_usuario`),
-  ADD KEY `id_departamento` (`id_departamento`);
+  ADD KEY `id_departamento` (`id_departamento`),
+  ADD KEY `departamento_destino` (`departamento_destino`);
 
 --
 -- Índices de tabela `departamentos`
@@ -231,22 +180,16 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de tabela `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
-
---
 -- AUTO_INCREMENT de tabela `chamados`
 --
 ALTER TABLE `chamados`
-  MODIFY `id_chamado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_chamado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `departamentos`
 --
 ALTER TABLE `departamentos`
-  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `feedbacks`
@@ -264,24 +207,19 @@ ALTER TABLE `tipos_usuario`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `categorias`
---
-ALTER TABLE `categorias`
-  ADD CONSTRAINT `categorias_ibfk_1` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`);
-
---
 -- Restrições para tabelas `chamados`
 --
 ALTER TABLE `chamados`
   ADD CONSTRAINT `chamados_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  ADD CONSTRAINT `chamados_ibfk_2` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`);
+  ADD CONSTRAINT `chamados_ibfk_2` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`),
+  ADD CONSTRAINT `chamados_ibfk_3` FOREIGN KEY (`departamento_destino`) REFERENCES `departamentos` (`id_departamento`);
 
 --
 -- Restrições para tabelas `feedbacks`
