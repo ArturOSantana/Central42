f1b6dc5c9e46d9c69ded0c0e39aad50edf854305
Artur O. Santana <artursantana123@gmail.com>
2025-11-22 13:12:11 -0300

    Adicionando os feedbacks

---
+++ classes/Feedback.php
@@ -0,0 +1,36 @@
+<?php 
+class Feedback{
+private $id_chamado;
+private $id_usuario;
+private $nota;
+private $comentario;
+private $data_feedback;
+
+
+
+
+public function __construct()
+{
+    
+}
+
+public function adicionarFeedback($id_chamado, $id_usuario, $nota, $comentario, $data_feedback){
+
+
+
+
+
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+?>
